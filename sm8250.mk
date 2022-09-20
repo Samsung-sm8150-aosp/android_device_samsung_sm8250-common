e744b5af024e5043be3b3a1b20a1fc34c523d5ed
sc-ah <aljoshua.hell@symblcrowd.de>
2022-09-20 12:22:27 +0200

    remove init.qcom.usb.rc from targets

--- sm8250.mk
+++ sm8250.mk
@@ -372,10 +372,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service-qti
 
-PRODUCT_PACKAGES += \
-    init.qcom.usb.rc \
-    init.qcom.usb.sh
-
 PRODUCT_SOONG_NAMESPACES += \
     vendor/qcom/opensource/usb/etc
 
