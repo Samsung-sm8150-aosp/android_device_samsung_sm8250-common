5d9bd7f9c675d33edc78bc19f8938a533f1a48bc
sc-ah <aljoshua.hell@symblcrowd.de>
2022-09-21 15:34:38 +0200

    satisfy sammy health impl deps

--- sm8250.mk
+++ sm8250.mk
@@ -141,8 +141,7 @@ PRODUCT_PACKAGES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-impl \
-    android.hardware.health@2.1-service
+    android.hardware.health@2.1.vendor
 
 
 # HIDL
