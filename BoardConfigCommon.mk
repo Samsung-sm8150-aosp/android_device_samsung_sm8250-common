cc93f10cc5662d8b3ae579b4a2363f780578a40d
sc-ah <aljoshua.hell@symblcrowd.de>
2022-09-23 09:38:19 +0200

    add odm prop

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -231,6 +231,7 @@ BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED := true
 TARGET_PRODUCT_PROP += $(COMMON_PATH)/product.prop
 TARGET_SYSTEM_PROP += $(COMMON_PATH)/system.prop
 TARGET_VENDOR_PROP += $(COMMON_PATH)/vendor.prop
+TARGET_ODM_PROP += $(COMMON_PATH)/odm.prop
 
 # Inherit the proprietary files
 include vendor/samsung/sm8250-common/BoardConfigVendor.mk
