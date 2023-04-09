ead4fba74cde47f7e568beef59ded51e8ff83536
xxTurborocketxx <70784636+xxTurborocketxx@users.noreply.github.com>
2023-04-09 21:55:04 +0200

    SM8250-common: fix typo

--- common.mk
+++ common.mk
@@ -446,6 +446,10 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/samsung/aidl/power-libperfmgr \
     hardware/samsung/
 
+#Thermnal
+    PRODUCT_PACKAGES += \
+        android.hardware.thermal@2.0-service.samsung.sm8250
+
 # Touch
 #PRODUCT_PACKAGES += \
  #   vendor.lineage.touch@1.0-service.samsung
